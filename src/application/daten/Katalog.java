e2d8fea1bea2208e230470ec7f44b601d3b257bf
Chunk3r <patrick.pirig@hhu.de>
2016-07-04 12:27:54 +0200

    added comments

--- src/application/daten/Katalog.java
+++
@@ -1,5 +0,0 @@
-package application.daten;
-
-public class Katalog {
-
-}
--- src/application/logik/Logik.java
+++ src/application/logik/Logik.java
@@ -5,33 +5,26 @@ import FileHandling.Exercise;
 public class Logik implements LogikZuGui{
 
 	@Override
-	public void save() {
-		// TODO Auto-generated method stub
+	public void save() { //Tests und Programmcode in Dateien speichern
+		
 		
 	}
 
 	@Override
-	public void loadKatalog() {
-		// TODO Auto-generated method stub
+	public void loadKatalog() { //Den katalog laden und tddt entsprechend einrichten
+		
 		
 	}
 
 	@Override
-	public void nextStep() {
-		if(isCompileable()){
+	public void nextStep() { //wenn der code kompiliert wird zum nächsten schritt gewechselt
+		if(){
 			
 		}
 		
 	}
 	
-	private boolean isCompileable(){
-		if(){
-			return true;
-		}
-		return false;
-	}
-	
-	private void countdown(long minuten){
+	private void countdown(long minuten){ //wenn aktiviert wird 
 		long t1 = 0;
 		long t2 = System.nanoTime();
 		long deltaT = t2 - t1;
