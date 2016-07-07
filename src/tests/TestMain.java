a52489d587dd19f7cb9c3949c6675ca81cfdf875
Chunk3r <patrick.pirig@hhu.de>
2016-07-07 12:49:26 +0200

    Compiliermethoden Tests hinzugefügt

--- src/tests/TestMain.java
+++ src/tests/TestMain.java
@@ -10,5 +10,37 @@ public class TestMain {
 	public void test() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testCodeCompilableTrue(){
+		Code c = new Code("Foo", "public class Foo{public static void main(String[] args){System.out.println("Hello World");}}");//c = richtiger Hello World testcode;
+		assertEquals(true, c.testingCompilationUnit());
+	}
+	
+	@Test
+	public void testCodeCompilableFalse(){
+		Code c = new Code("Foo", "dieser code laeuft nicht");
+		assertEquals(false, c.testingCompilationUnit());
+	}
+	
+	@Test
+	public void testCodeListCompilableTrue(){
+		CodeList<Code> l = new CodeList<Code>();
+		Code c1 = new Code("Foo", "public class Foo{public static void main(String[] args){System.out.println("Hello World");}}");
+		Code c2 = new Code("Bar", "public class Foo{public static void main(String[] args){System.out.println("Hello World");}}");
+		l.add(c1);
+		l.add(c2);
+		assertEquals(true, l.compilable());
+	}
+	
+	@Test
+	public void testCodeListCompilableFalse(){
+		CodeList<Code> l = new CodeList<Code>();
+		Code c1 = new Code("Foo", "public class Foo{public static void main(String[] args){System.out.println("Hello World");}}");//richtiger code todo
+		Code c2 = new Code("Bar", "dieser code laeuft nicht");//falscher code
+		l.add(c1);
+		l.add(c2);
+		assertEquals(false, l.compilable());
+	}
+	
 }
