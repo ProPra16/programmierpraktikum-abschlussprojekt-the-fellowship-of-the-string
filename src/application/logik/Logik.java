390db3277cd5dae0e79cea35d2ee6b5e82eb6c47
Chunk3r <patrick.pirig@hhu.de>
2016-07-12 18:06:31 +0200

    Update Logik.java

--- src/application/logik/Logik.java
+++ src/application/logik/Logik.java
@@ -18,6 +18,7 @@ import FileHandling.Exercise;
 public class Logik implements LogikZuGui{
 	
 	boolean lauft = true;
+	int step = 0; //0 = test schreiben, 1 = code schreiben, 2 = refactor;
 	Exercise e = new Exercise();
 	
 	@Override
@@ -33,9 +34,25 @@ public class Logik implements LogikZuGui{
 	}
 
 	@Override
-	public void nextStep() { //wenn der code kompiliert, wird zum nächsten schritt gewechselt
-		if(e.codeCompiles()){
-			Controller.SwitchArea();//Die GUI methode zum textfeld wechseln wird aufgerufen
+	public void nextStep() {
+		switch(step)
+		case 0: {
+			if(code.codeCompiles() && test.einTestSchleagtFehl()){ //wenn tests kompilieren weiter
+				Controller.SwitchArea();//Die GUI methode zum textfeld wechseln wird aufgerufen
+				step = 1;
+			}
+			break;
+		}
+		case 1:{
+			if(code.codeCompiles() && test.fehlerfrei()){//wenn
+				step = 2;
+			}
+		}
+		case 2:{
+			if(code.codeCompiles() && test.fehlerfrei()){
+				Controller.SwitchArea();
+				step = 0;
+			}
 		}
 		
 	}
