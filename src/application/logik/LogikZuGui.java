2863f5fa7a330610c2af094a9aa31ed515b49e2c
Chunk3r <patrick.pirig@hhu.de>
2016-07-12 15:00:35 +0200

    Update LogikZuGui.java

--- src/application/logik/LogikZuGui.java
+++ src/application/logik/LogikZuGui.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2016 The Fellowship of the String and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marcel Beek - initial API and implementation
+ *     Patrick Pirig - initial API and implementation
+ *     Phillippe Weise - initial API and implementation
+ *     Sabine Timmer - initial API and implementation
+ *******************************************************************************/
 package application.logik;
 
 public interface LogikZuGui {
