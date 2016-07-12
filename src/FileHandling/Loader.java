d2b8f38e4fbaf10e79f4021cb07fcb2fba45a548
phwei <phwei104@uni-duesseldorf.de>
2016-07-12 15:04:47 +0200

    anpassungen an dtd und Hauptfenster vom loader geht jetzt.

--- src/FileHandling/Loader.java
+++ src/FileHandling/Loader.java
@@ -15,10 +15,14 @@ package FileHandling;
 
 import org.xml.sax.*;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import org.w3c.dom.*;
@@ -29,18 +33,32 @@ import javax.xml.parsers.*;
 
 public class Loader {
 
-	public Exercise loadExcercise() {
+	public static Exercise loadExcercise() {
 		Stage loadStage = new Stage();
 		TextField fileName = new TextField("Katalogdatei");
-		Label anzeige = new Label("");
+		fileName.setMaxWidth(100);
+		
+		Label anzeige = new Label("anzeige");
 
 		Button laden = new Button("Katalog laden");
 		laden.setOnAction(e -> laden(fileName.getText(), anzeige, loadStage));
-
+		
+		VBox vBox=new VBox(10);
+		vBox.getChildren().add(anzeige);
+		vBox.getChildren().add(fileName);
+		vBox.getChildren().add(laden);
+		vBox.setAlignment(Pos.CENTER);
+		vBox.setPadding(new Insets(20,20,20,20));		
+		
+		Scene loadScene=new Scene(vBox,300,100);
+		loadStage.setScene(loadScene);
+		loadStage.show();
+		
+		
 		return null;
 	}
 
-	private void laden(String filename, Label anzeige, Stage stage) {
+	private static void laden(String filename, Label anzeige, Stage stage) {
 		try {
 			Document xmldoc = loadDoc(filename);
 			Scene choiseScreen = katalogView(xmldoc);
@@ -52,12 +70,12 @@ public class Loader {
 
 	}
 
-	private Scene katalogView(Document xmldoc) {
+	private static Scene katalogView(Document xmldoc) {
 
 		return null;
 	}
 
-	private Document loadDoc(String fileLocation) throws SAXException, IOException {
+	private static Document loadDoc(String fileLocation) throws SAXException, IOException {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		Document toReturn;
 		factory.setIgnoringComments(true);
@@ -72,4 +90,15 @@ public class Loader {
 			return null;
 		}
 	}
+	private static Exercise[] parseExercises(Document xmldoc){
+		NodeList exerList=xmldoc.getElementsByTagName("Exercise");
+		Exercise[] exercArray=new Exercise[exerList.getLength()];
+		
+		for(int i=0;i<exercArray.length;i++){
+			
+		}
+	}
+	
+	
+	
 }
