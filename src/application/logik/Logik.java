059cf7449b339f0512c4404daa03e70dc77e5e39
Chunk3r <patrick.pirig@hhu.de>
2016-07-13 10:32:55 +0200

    added tracking
    trackStart muss am anfang noch aufgerufen werden!!

--- src/application/logik/Logik.java
+++ src/application/logik/Logik.java
@@ -19,6 +19,7 @@ public class Logik implements LogikZuGui{
 	
 	boolean lauft = true;
 	int step = 0; //0 = test schreiben, 1 = code schreiben, 2 = refactor;
+	long tr1 = 0, tr2 = 0, trT = 0, trC = 0, trR = 0;
 	Exercise e = new Exercise();
 	
 	@Override
@@ -39,25 +40,31 @@ public class Logik implements LogikZuGui{
 		case 0: {
 			if(code.codeCompiles() && test.oneTestFailing()){ //wenn tests kompilieren weiter
 				Controller.SwitchArea();//Die GUI methode zum textfeld wechseln wird aufgerufen
+				trT += trackStop();
+				trackStart();
 				step = 1;
 			}
 			break;
 		}
 		case 1:{
 			if(code.codeCompiles() && test.allTestsPassing()){//wenn compiliert und alle tests laufen weiter
+				trC += trackStop();
+				trackStart();
 				step = 2;
 			}
 		}
 		case 2:{
 			if(code.codeCompiles() && test.allTestsPassing()){//wenn immernoch alles laeuft weiter
 				Controller.SwitchArea();
+				trR += trackStop();
+				trackStart();
 				step = 0;
 			}
 		}
 		
 	}
 	
-
+	//fuer babysteps
 	private void countdown(long sekunden){ //wenn aktiviert, wird die übergebene zeit bis null runtergezählt
 		lauft = true;
 		long deltaT = System.nanoTime();
@@ -65,6 +72,17 @@ public class Logik implements LogikZuGui{
 			deltaT += System.nanoTime()/1000000000;
 		}
 		lauft = false;
+	}
+	
+	//fuer tracking
+	// TODO trackStart() muss am anfang mit aufgerufen werden!!
+	private void trackStart(){
+		tr1 = System.nanoTime()/1000000000;
+	}
+	
+	private long trackStop(){
+		tr2 = System.nanoTime()/1000000000;
+		return tr2 - tr1;
 	}
 	
 }
