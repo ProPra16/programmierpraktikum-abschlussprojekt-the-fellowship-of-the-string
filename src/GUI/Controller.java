5bbda1cc69b07e780db23dd0e3f851eecbb8e103
Maz3l <marcel_be95@gmx.de>
2016-07-14 23:19:15 +0200

    logik in controller

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -7,10 +7,10 @@ import application.logik.Logik;
 
 public class Controller {
 
-	@FXML
-	private TextArea Area1;
-	@FXML
-	private TextArea Area2;
+    @FXML
+    private TextArea Area1;
+    @FXML
+    private TextArea Area2;
     @FXML
     private Button bRed;
     @FXML
@@ -25,6 +25,7 @@ public class Controller {
     @FXML
     public void initialize(){
         e = new Exercise();
+        log = new Logik(e);
         Area1.setText(e.getCode());
         Area2.setText(e.getTest());
         Loader.loadExcercise(e);
@@ -40,53 +41,59 @@ public class Controller {
     //}
 
 
-	public void SwitchArea() {
-		if (Area1.isDisable()) {
-			Area1.setDisable(false);
-			Area2.setDisable(true);
-			Area1.requestFocus();
-		} else {
-			Area2.setDisable(false);
-			Area1.setDisable(true);
-			Area2.requestFocus();
-		}
-	}
+    public void SwitchArea() {
+        if (Area1.isDisable()) {
+            Area1.setDisable(false);
+            Area2.setDisable(true);
+            Area1.requestFocus();
+        } else {
+            Area2.setDisable(false);
+            Area1.setDisable(true);
+            Area2.requestFocus();
+        }
+    }
 
     // Buttons verändern
     @FXML 
-	protected void toRed() {
-        ampel.wechselZuRot(true);
-        SwitchArea();
-        // Button aktivieren
-        bGreen.setDisable(false);
+    protected void toRed() {
+        if (log.nextStep()) {
+            ampel.wechselZuRot(true);
+            SwitchArea();
+            // Button aktivieren
+            bGreen.setDisable(false);
 
-        // Buttons deaktivieren
-        bYellow.setDisable(true);
-        bRed.setDisable(true);
+            // Buttons deaktivieren
+            bYellow.setDisable(true);
+            bRed.setDisable(true);
+        }
     }
 
     @FXML
     protected void toGreen() {
-        ampel.wechselZuGruen(true);
-        SwitchArea();
-        // Button aktivieren
-        bYellow.setDisable(false);
+        if (log.nextStep()) {
+            ampel.wechselZuGruen(true);
+            SwitchArea();
+            // Button aktivieren
+            bYellow.setDisable(false);
 
-        // Buttons deaktivieren
-        bGreen.setDisable(true);
-        bRed.setDisable(true);
-	}
+            // Buttons deaktivieren
+            bGreen.setDisable(true);
+            bRed.setDisable(true);
+        }
+    }
 
     @FXML
     protected void toYellow() {
-        ampel.wechselZuGelb(true);
-        //SwitchArea();
-        // Button aktivieren
-        bRed.setDisable(false);
+        if (log.nextStep()) {
+            ampel.wechselZuGelb(true);
+            //SwitchArea();
+            // Button aktivieren
+            bRed.setDisable(false);
 
-        // Buttons deaktivieren
-        bYellow.setDisable(true);
-        bGreen.setDisable(true);
+            // Buttons deaktivieren
+            bYellow.setDisable(true);
+            bGreen.setDisable(true);
+        }
     }
 
     // Ampel referenz übergeben
