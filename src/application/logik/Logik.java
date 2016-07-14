0b7cecdc4eeb024f126789c8c7515071ca641f17
sabine12345 <sabine.timmer@uni-duesseldorf.de>
2016-07-14 12:26:38 +0200

    piechart für tracking

--- src/application/logik/Logik.java
+++ src/application/logik/Logik.java
@@ -1,6 +1,12 @@
 // Copyright (c) <2016> <Marcel Beek, Patrick Pirig, Phillippe Weise, Sabine Timmer>
 package application.logik;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.ui.RefineryUtilities;
+
 import FileHandling.Exercise;
 import GUI.Controller;
 
@@ -8,14 +14,17 @@ public class Logik implements LogikZuGui{
 	
 	boolean lauft = true;
 	int step = 0; //0 = test schreiben, 1 = code schreiben, 2 = refactor;
-	private final int SEC = 1000000000;
-	long tr1 = 0, tr2 = 0, trT = 0, trC = 0, trR = 0;
+	private final static int SEC = 1000000000;
+	static long tr1 = 0;
+	static long tr2 = 0;
+	long trT = 0;
+	long trC = 0;
+	long trR = 0;
 	Exercise e = new Exercise();
 	
 	@Override
 	public void save() { //Tests und Programmcode in Dateien speichern
-		
-		
+
 	}
 
 	@Override
@@ -79,9 +88,25 @@ public class Logik implements LogikZuGui{
 		tr1 = System.nanoTime()/SEC;
 	}
 	
-	private long trackStop(){
+	private static long trackStop(){
 		tr2 = System.nanoTime()/SEC;
 		return tr2 - tr1;
+	}
+	
+	public void stats(){
+		//http://www.math.hu-berlin.de/~ccafm/lehre_BZQ_Numerik/allg/JAVA_Pakete/JFreeChart/Codes/PieChart_code.html
+		//JFreeChart Library (GNU License)
+		DefaultPieDataset pieDataset = new DefaultPieDataset();
+		pieDataset.setValue("Red", 25);
+		pieDataset.setValue("Refactor", 25);
+		pieDataset.setValue("Green", 50);
+		
+		JFreeChart chart = ChartFactory.createPieChart
+		("Time",pieDataset,true,false,false);
+		ChartFrame frame = new ChartFrame("Chart", chart);
+		frame.pack();
+		RefineryUtilities.centerFrameOnScreen(frame);
+		frame.setVisible(true);
 	}
 	
 }
